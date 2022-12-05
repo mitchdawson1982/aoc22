e1214c364301a17bf169e1d2ff8d0dd0b36b76e5
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-12-05 11:52:15 +0000

    :technologist: Automate each pythonic day

---
+++ scripts/main.py
@@ -0,0 +1,19 @@
+def part_1():
+    with open("input.txt", "r") as file:
+        data = file.read()
+        lines = data.split("\n\n")
+
+
+def part_2():
+    with open("input.txt", "r") as file:
+        data = file.read()
+        lines = data.split("\n\n")
+
+
+
+def main():
+    part_1()
+    # part_2()
+
+if __name__ == "__main__":
+    main()
